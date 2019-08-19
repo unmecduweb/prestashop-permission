3b4dd27b4fe93135d7b0d11a02f5939308bfe6c9
Fab <fabien@unmecduweb.com>
2019-08-19 18:12:18 +0200

    Init file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# prestashop-permission
+# Prestashop permission
+Script bash to set the right permissions when your install an existing prestashop project on localhost.
+
+- Create folder if not exists
+- Update the permission to 775 
+- Set the owner to [fab] and www-data
+
+
+### NB : change the username [fab] with your's
+### Script no test in production, only use in local
---
+++ setPermissions.sh
@@ -0,0 +1,26 @@
+
+files=( "app/logs" \
+"app/config" \
+"app/Resources/translations" \
+"cache/" \
+"config" \
+"config/themes" \
+"download" \
+"img" \
+"log" \
+"mails" \
+"modules" \
+"themes" \
+"translations" \
+"var" \
+ )
+
+for i in "${files[@]}"
+do
+   :
+   echo $i
+    mkdir -p $i;
+    sudo chmod 775 $i -R;
+    sudo chown fab:www-data $i -R;
+    echo "$i is set";
+done
